c1f29d94cc6881672186af557a01811261c20631
jlelliott94 <jlelliott94@users.noreply.github.com>
2019-12-28 23:44:03 +1100

    testing

---
+++ AZ-103 Command List.ps1
@@ -0,0 +1,13 @@
+#CTRL SHIFT P TO CHANGE LANGUAGE
+
+#### Skillpipe ####
+
+## Backfill commands that I haven't added yet! ##
+
+# Module 1 Demo 5 - 
+
+# New Resource Lock
+New-AzResourceLock -LockName Posh-Delete-Lock -LockLevel CanNotDelete -ResourceGroupName Mod1Demo5-RG
+
+# Get all Resource Locks
+Get-AzResourceLock
