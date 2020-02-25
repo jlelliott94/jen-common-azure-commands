57b6f033892415df7021bc8ade06db596efd28c5
jlelliott94 <jlelliott94@users.noreply.github.com>
2020-02-25 23:09:23 +1100

    commit

--- AZ-103 Command List.ps1
+++ AZ-103 Command List.ps1
@@ -211,3 +211,60 @@ $location = "eastus"
 $resourceGroup = "mod3demo1-rg" 
 New-AzResourceGroup -Name $resourceGroup -Location $location 
 New-AzStorageAccount -ResourceGroupName $resourceGroup -Name "mod3demo1" -Location $location -SkuName Standard_LRS -Kind StorageV2 
+
+# Create File Share
+# Retrieve storage account and storage account key
+$storageContext = New-AzStorageContext <storage-account-name> <storage-account-key>
+# Create the file share, in this case “logs”
+$share = New-AzStorageShare logs -Context $storageContext
+
+# Enable Secure Transfer Required
+et-AzStorageAccount -Name <StorageAccountName> -ResourceGroupName <ResourceGroupName> -EnableHttpsTrafficOnly $True
+
+# Creat File Share, Retrieve Access Key, Create Context and Create File Share (Demo 4)
+# Gather storage acc name and key
+Get-AzStorageAccount | fl *name*
+Get-AzStorageAccount -ResourceGroupName "YourResourceGroupName" -Name "YourStorageAccountName"
+
+# retrieve access key for storage account
+$storageAccountKeys = Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName
+
+#Create context for storage account/key
+$storageContext = New-AzStorageContext -StorageAccountName "YourStorageAccountName" -StorageAccountKey $storageAccountKeys[0].value
+
+#Create File Share
+$share = New-AzStorageShare "YourFileShareName" -Context $storageContext
+
+#Mount file share
+$resourceGroupName = "your-resource-group-name"
+$storageAccountName = "your-storage-account-name"
+$fileShareName = "your-file-share-name"
+
+# These commands require you to be logged into your Azure account, run Login-AzAccount if you haven't
+# already logged in.
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName
+$storageAccountKeys = Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName
+$fileShare = Get-AzStorageShare -Context $storageAccount.Context | Where-Object { 
+    $_.Name -eq $fileShareName -and $_.IsSnapshot -eq $false
+}
+
+if ($fileShare -eq $null) {
+    throw [System.Exception]::new("Azure file share not found")
+}
+
+# The value given to the root parameter of the New-PSDrive cmdlet is the host address for the storage account, 
+# storage-account.file.core.windows.net for Azure Public Regions. $fileShare.StorageUri.PrimaryUri.Host is 
+# used because non-Public Azure regions, such as sovereign clouds or Azure Stack deployments, will have different 
+# hosts for Azure file shares (and other storage resources).
+$password = ConvertTo-SecureString -String $storageAccountKeys[0].Value -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "AZURE\$($storageAccount.StorageAccountName)", $password
+New-PSDrive -Name desired-drive-letter -PSProvider FileSystem -Root "\\$($fileShare.StorageUri.PrimaryUri.Host)\$($fileShare.Name)" -Credential $credential -Persist
+
+#dismount file share
+Remove-PSDrive -Name desired-drive-letter
+
+# Create storage account level SAS with full permissions
+New-AzStorageAccountSASToken -Service Blob,File,Table,Queue -ResourceType Service,Container,Object -Permission "racwdlup"
+
+# Create blob level SAS with full permissions
+New-AzStorageBlobSASToken -Container "ContainerName" -Blob "BlobName" -Permission rwd
