8e80a527dfb35c526df3bbc8ce3602b8b3809bab
Jennifer Elliott <jlelliott94@outlook.com>
2020-05-02 15:06:40 +1000

    more-amendments

--- Scripts/Create-2vNets-2Subnets/Create-2vNets-2Subnets.ps1
+++ Scripts/Create-2vNets-2Subnets/Create-2vNets-2Subnets.ps1
@@ -25,12 +25,12 @@ $vnet1subnet1 = New-AzVirtualNetworkSubnetConfig -Name Subnet1 -AddressPrefix "1
 # New GW subnet for vNet 1
 $vnet1gwsubnet = New-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -AddressPrefix "10.0.0.128/25"
 # Create vNet1, associate to RG, associate subnets
-$vnet1 = New-AzVirtualNetwork -Name vNet1 -ResourceGroupName "mod5demo2-rg" -Location "australiaeast" -AddressPrefix "10.0.0.0/24" -Subnet $vnet1subnet1,$vnet1gatewaysubnet
+$vnet1 = New-AzVirtualNetwork -Name vNet1 -ResourceGroupName "mod5demo2-rg" -Location "australiaeast" -AddressPrefix "10.0.0.0/24" -Subnet $vnet1subnet1,$vnet1gwsubnet
 
 # New Subnet for vNet2
 $vnet2subnet1 = New-AzVirtualNetworkSubnetConfig -Name Subnet1 -AddressPrefix "10.0.1.0/25"
 # New GW subnet for vNEt2
 $vnet2gwsubnet = New-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -AddressPrefix "10.0.1.128/25"
 # Create vNet2, associate it to RG, associate subnets
-$vnet2 = New-AzVirtualNetwork -Name vNet1 -ResourceGroupName "mod5demo2-rg" -Location "australiaeast" -AddressPrefix "10.0.1.0/24" -Subnet $vnet1subnet1,$vnet1gatewaysubnet
+$vnet2 = New-AzVirtualNetwork -Name vNet1 -ResourceGroupName "mod5demo2-rg" -Location "australiaeast" -AddressPrefix "10.0.1.0/24" -Subnet $vnet2subnet1,$vnet2gwsubnet
 
